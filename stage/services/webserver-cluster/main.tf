f758d45d1368bc646ab137da0562d4ce697f39ce
AnastasiaKolo <kolodochko@gmail.com>
2020-11-04 10:13:41 +0300

    file layout feractoring

--- stage/services/webserver-cluster/main.tf
+++ stage/services/webserver-cluster/main.tf
@@ -11,16 +11,6 @@ terraform {
   }
 }
 
-variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
-
-output "alb_dns_name" {
-  value       = aws_lb.example.dns_name
-  description = "The domain name of the load balancer"
-}
 
 provider "aws" {
   region = "us-east-2"
